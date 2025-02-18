88bdffcd2c7a84ad2342d1264901b39adaecc9e6
lgnehc <61610458+lgnehc@users.noreply.github.com>
2025-02-19 00:36:28 +0800

    Add files via upload

---
+++ src_66/include/onix/memory.h
@@ -0,0 +1,66 @@
+#ifndef ONIX_MEMORY_H
+#define ONIX_MEMORY_H
+
+#include <onix/types.h>
+
+#define PAGE_SIZE 0X1000      //一页大小 4K
+#define MEMORY_BASE 0X100000  //1M开始内存位置
+
+//内核占用的内存大小8M
+#define KERNEL_MEMORY_SIZE 0X800000
+//用户栈顶地址 128M
+#define USER_STACK_TOP     0X8000000
+
+//定义用户栈最大2M
+#define USER_STACK_SIZE 0X200000
+//定义用户栈底
+#define USER_STACK_BOTTOM  (USER_STACK_TOP - USER_STACK_SIZE)
+
+//页目录项和页表项映射位置,映射8M
+#define KERNEL_PAGE_DIR   0X1000
+/*//内核页表索引
+static u32 KERNEL_PAGE_TABLE[] = {
+    0X2000,
+    0X3000,
+};*/
+
+
+//页表项结构体
+typedef struct page_entry_t
+{
+    u8 present : 1;  // 是否映射在内存中
+    u8 write : 1;    // 0 只读 1 可读可写
+    u8 user : 1;     // 1 所有人 0 超级用户 DPL < 3
+    u8 pwt : 1;      // page write through 1 直写模式，0 回写模式
+    u8 pcd : 1;      // page cache disable 禁止该页缓冲
+    u8 accessed : 1; // 被访问过，用于统计使用频率
+    u8 dirty : 1;    // 脏页，表示该页缓冲被写过
+    u8 pat : 1;      // page attribute table 页大小 4K/4M
+    u8 global : 1;   // 全局，所有进程都用到了，该页不刷新缓冲
+    u8 shared : 1;   // 共享内存页，与 CPU 无关
+    u8 privat : 1;   // 私有内存页，与 CPU 无关
+    u8 readonly : 1; // 只读内存页，与 CPU 无关
+    u32 index : 20;  // 页索引
+} _packed page_entry_t;
+
+void memory_init(u32 magic , u32 addr);
+
+void memory_map_init();
+void mapping_init();
+//void memory_test();
+
+u32 get_cr2();            // 得到 cr2 寄存器,缺页异常时造成缺页的指令地址
+
+u32 get_cr3();            // 得到 cr3 寄存器
+void set_cr3(u32 pde);  // 设置 cr3 寄存器，参数是页目录的地址
+
+
+u32 alloc_kpage(u32 count); //分配连续count个内核页面
+void free_kpage(u32 vaddr,u32 count); //释放连续count个内核页面
+
+void link_page(u32 vaddr);    //将vaddr映射物理内存
+void unlink_page(u32 vaddr);  //取消vaddr对应的物理内存映射
+
+
+
+#endif
---
+++ src_66/kernel/thread.c
@@ -0,0 +1,77 @@
+#include <onix/interrupt.h>
+#include <onix/syscall.h>
+#include <onix/debug.h>
+#include <onix/mutex.h>
+#include <onix/printk.h>
+#include <onix/task.h>
+#include <onix/stdio.h>
+#include <onix/arena.h>
+
+//extern int printf(const char *fmt, ...);
+
+void idle_thread()
+{
+    set_interrupt_state(true);
+    u32 count = 0;
+    while (true)
+    {
+        //LOGK("idle task..... %d",count++);
+        asm volatile(
+            "sti\n"     //开启中断
+            "hlt\n"     //关闭cpu,进入暂停状态,等待外中断的到来
+        );
+        yield();        //放弃执行权,调度其他程序
+    }
+}
+
+
+//mutex_t mutex;
+lock_t lock;
+extern u32 keyboard_read(char *buf,u32 count);
+
+
+static void  real_init_thread()
+{
+    u32 count = 0;
+    char ch;
+    while(true)
+    {
+        //BMB;
+        //asm volatile("in $0x92,%ax\n");
+        test();
+        sleep(1000);
+        //printf("task is in user mode %d\n",count++);
+        //printk("task is in user mode %d\n",count++);
+    }
+}
+
+
+void init_thread()
+{
+    char temp[100];
+    task_to_user_mode(real_init_thread);
+   
+}
+
+
+void test_thread()
+{
+    set_interrupt_state(true);
+    u32 counter = 0;
+    while (true)
+    {
+        void *ptr = kmalloc(1200);
+        LOGK("kmalloc 0x%p...",ptr);
+        kfree(ptr);
+
+        ptr = kmalloc(1024);
+        LOGK("kmalloc 0x%p...",ptr);
+        kfree(ptr);
+        
+        ptr = kmalloc(54);
+        LOGK("kmalloc 0x%p...",ptr);
+        kfree(ptr);
+
+        sleep(5000) ;
+    }
+}
