10e71fa686f93f30a5c8185096b300f73999270b
lgnehc <61610458+lgnehc@users.noreply.github.com>
2025-02-19 15:53:57 +0800

    Add files via upload

---
+++ src_69/include/onix/syscall.h
@@ -0,0 +1,29 @@
+#ifndef ONIX_SYSCALL_H
+#define ONIX_SYSCALL_H
+
+#include <onix/types.h>
+
+typedef enum syscall_t
+{
+    SYS_NR_TEST,
+    SYS_NR_WRITE = 4,
+    SYS_NR_GETPID = 20,
+    SYS_NR_BRK = 45,
+    SYS_NR_GETPPID = 64,
+    SYS_NR_YIELD = 158,
+    SYS_NR_SLEEP = 162,
+}syscall_t;
+
+u32 test();
+void yield();
+void sleep(u32 ms);
+
+pid_t getpid();
+pid_t getppid();
+
+int32 write(fd_t fd,char *buf, u32 len);
+
+int32 brk(void *addr);
+
+
+#endif
