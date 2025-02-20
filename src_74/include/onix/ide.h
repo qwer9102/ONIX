d68957d1d040ce3e3926bbba9e3c544266fc6b68
lgnehc <61610458+lgnehc@users.noreply.github.com>
2025-02-20 16:34:23 +0800

    Add files via upload

---
+++ src_74/include/onix/ide.h
@@ -0,0 +1,37 @@
+#ifndef ONIX_IDE_H
+#define ONIX_IDE_H
+
+#include <onix/types.h>
+#include <onix/mutex.h>
+
+#define SECTOR_SIZE 512     // 扇区大小
+//#define CD_SECTOR_SIZE 2048 // 光盘扇区大小
+
+#define IDE_CTRL_NR 2 // 控制器数量，固定为 2
+#define IDE_DISK_NR 2 // 每个控制器可挂磁盘数量，固定为 2
+
+
+// IDE 磁盘
+typedef struct ide_disk_t
+{
+    char name[8];                  // 磁盘名称
+    struct ide_ctrl_t *ctrl;       // 这个磁盘是哪个控制器指针控制的
+    u8 selector;                   // 选择主从磁盘以及模式
+    bool master;                   // 是否是主盘
+} ide_disk_t;
+
+// IDE 控制器
+typedef struct ide_ctrl_t
+{
+    char name[8];                  // 控制器名称
+    lock_t lock;                   // 控制器锁
+    u16 iobase;                    // IO 寄存器基址,加上offset可以访问到其他寄存器
+    ide_disk_t disks[IDE_DISK_NR]; // 磁盘
+    ide_disk_t *active;            // 当前选择的磁盘
+} ide_ctrl_t;
+
+//硬盘同步PIO读写
+int ide_pio_read(ide_disk_t *disk, void *buf, u8 count, idx_t lba);
+int ide_pio_write(ide_disk_t *disk, void *buf, u8 count, idx_t lba);
+
+#endif
